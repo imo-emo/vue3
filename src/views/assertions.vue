f7f0527e0cfd2e5f44654196ff4e346928d6cce5
suzhiyu <suzhiyu@abitai.com>
2019-07-30 11:35:03 +0800

    Symbol

--- src/views/assertions.vue
+++ src/views/assertions.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="assertions">
         assertions
+        {{GetName}}
+        <input v-model="name" />
         <back></back>
     </div>
 </template>
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
+import {Component, Vue, Provide, Watch} from 'vue-property-decorator';
 import back from '../components/back.vue';
 
 @Component({
@@ -14,9 +16,30 @@ import back from '../components/back.vue';
     },
 })
 class Assertions extends Vue {
+    @Provide() private name: string = '123';
     public mounted(): void {
         const name = `Smallblack`;
         const len = (name as string).length;
+        this.doSomething();
+        const obj = {
+            [Symbol('name')]: '一斤代码',
+            age: 18,
+        };
+    }
+    public async doSomething() {
+        const result = this.getSomething();
+    }
+    public getSomething() {
+        return 'abc';
+    }
+    get GetName() {
+        return this.name.split('').reverse().join('');
+    }
+    @Watch('name') public nameChange() {
+        const name = this.name.slice(this.name.length - 3, this.name.length);
+        this.$nextTick(() => {
+            this.name = name;
+        });
     }
 }
 
